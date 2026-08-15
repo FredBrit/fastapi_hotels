febafa3fb2980240525ba14d1a5a315f682158fe
root <root@jgxfnvztql.local>
2026-08-15 10:35:56 +0000

    1508

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -2,6 +2,7 @@ import json
 from fastapi import APIRouter
 from fastapi_cache.decorator import cache
 from src.schemas.facilities import FacilityAdd
+from src.tasks.tasks import test_task
 from src.api.dependencies import DBDep
 from src.init import redis_manager
 
@@ -22,4 +23,7 @@ async def get_facilities(db: DBDep):
 async def create_facility(db: DBDep, facility_data: FacilityAdd):
     facility = await db.facilities.add(facility_data)
     await db.commit()
+
+    test_task.delay()
+
     return {'status': 'OK', 'data': facility}    
